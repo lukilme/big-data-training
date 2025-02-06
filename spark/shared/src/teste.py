c30ec27c6aa82744ec41cc1eaa960c5b2f2b48d3
lukilme <kilmerluisluk12@gmail.com>
2025-02-06 11:22:50 -0300

    spark update

--- spark/shared/src/teste.py
+++ spark/shared/src/teste.py
@@ -1,1 +1,6 @@
-print("work")
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("spark://172.17.0.2:7077").appName("Test").getOrCreate()
+df = spark.range(130).toDF("number")
+df.show()
+spark.stop()
